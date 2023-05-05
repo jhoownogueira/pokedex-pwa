4e7d3c68f97386be60a588520eeb6a4e4571cbc5
jhonata <jhoownogueira@outlook.com>
2023-05-05 18:32:23 -0300

    removendo configurações desnecessárias

--- next.config.js
+++ next.config.js
@@ -14,18 +14,6 @@ const withPWA = require('next-pwa')({
     },
     // Outras configurações de cache, se necessário
   ],
-  ios: {
-    'apple-mobile-web-app-title': 'Pokédex',
-    'apple-mobile-web-app-capable': 'yes',
-    'apple-mobile-web-app-status-bar-style': 'default',
-    'apple-touch-icon': [
-      { size: '128x128', href: '/icons/128.png' },
-      { size: '144x144', href: '/icons/144.png' },
-      { size: '152x152', href: '/icons/152.png' },
-      { size: '180x180', href: '/icons/180.png' }
-    ],
-    'apple-touch-icon-precomposed': '/icons/152.png',
-  },
 })
 
 module.exports = withPWA({
